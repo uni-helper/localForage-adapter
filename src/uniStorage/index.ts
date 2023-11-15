c6889ee83ec816be094ff115d33d3de449b7520f
江程训 <dfjsiou@qq.com>
2023-11-16 01:08:02 +0800

    暂存更改

--- src/uniStorage/index.ts
+++ src/uniStorage/index.ts
@@ -1,5 +1,6 @@
 import serializer from "localforage/src/utils/serializer";
 import executeCallback from "localforage/src/utils/executeCallback";
+import normalizeKey from 'localforage/src/utils/normalizeKey';
 
 /**
  * @description 在此库中定义uni是什么（uni-app的全局对象）
@@ -34,6 +35,7 @@ function support(): boolean {
 
 /**
  * @description 初始化storage
+ * @param options
  */
 function initStorage(options) {
   var self = this;
@@ -57,6 +59,95 @@ function initStorage(options) {
 }
 
 /**
+ * @description 清空当前库的所有数据
+ * @param callback 
+ */
+function clear(callback) {
+  var self = this;
+  var promise = self.ready().then(function () {
+    var keyPrefix = self._dbInfo.keyPrefix;
+    try {
+      const res = uni.clearStorageSync();
+      const keys = res.keys
+
+      for (var i = keys.length - 1; i >= 0; i--) {
+        if (keys[i].indexOf(keyPrefix) === 0) {
+          uni.removeStorageSync(keys[i]);
+        }
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  });
+
+  executeCallback(promise, callback);
+  return promise;
+}
+
+/**
+ * @description 获取当前库的所有数据
+ * @param key 
+ * @param callback 
+ * @returns 
+ */
+function getItem(key, callback) {
+  var self = this;
+
+  key = normalizeKey(key);
+
+  var promise = self.ready().then(function () {
+    var dbInfo = self._dbInfo;
+    try {
+      var result = uni.getStorageSync(dbInfo.keyPrefix + key);
+
+      if (result) {
+        result = dbInfo.serializer.deserialize(result);
+      }
+
+      return result;
+    } catch (e) {
+      return undefined
+    }
+  });
+
+  executeCallback(promise, callback);
+  return promise;
+}
+
+function iterate(iterator, callback) {
+  var self = this;
+
+  var promise = self.ready().then(function () {
+    var keyPrefix = self._dbInfo.keyPrefix;
+    var keyPrefixLength = keyPrefix.length;
+    var length = uni.getStorageInfoSync().keys.length;
+    var keys = uni.getStorageInfoSync().keys;
+    var iterationNumber = 1;
+
+    for (var i = 0; i < length; i++) {
+      var key = keys[i];
+      if (key.indexOf(keyPrefix) !== 0) {
+        continue;
+      }
+      var value = uni.getStorageSync(key);
+
+      if (value) {
+        value = self._dbInfo.serializer.deserialize(value);
+      }
+
+      value = iterator(value, key.substring(keyPrefixLength), iterationNumber++);
+
+      if (value !== void 0) {
+        return value;
+      }
+    }
+  });
+
+  executeCallback(promise, callback);
+  return promise;
+}
+
+/**
  * @description 最终的驱动器成品
  */
 export const uniStorageDriver = {
@@ -65,6 +156,8 @@ export const uniStorageDriver = {
   // 是否支持
   _support: support,
   _initStorage: initStorage,
+  clear: clear,
+  getItem: getItem,
 }
 
 export default uniStorageDriver;
